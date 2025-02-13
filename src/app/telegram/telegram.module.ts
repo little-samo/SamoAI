fa511e0f063acc011f70c798f5e926d7dc66923f
Lucid <lucid@littlesamo.io>
2025-02-13 12:23:23 +0900

    feat: add AgentsModule and UsersModule imports to TelegramModule

--- src/app/telegram/telegram.module.ts
+++ src/app/telegram/telegram.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AgentsModule } from '@app/agents/agents.module';
+import { UsersModule } from '@app/users/users.module';
 
 import { TelegramService } from './telegram.service';
 import { TelegramController } from './telegram.controller';
 
 @Module({
+  imports: [AgentsModule, UsersModule],
   providers: [TelegramService],
   controllers: [TelegramController],
   exports: [TelegramService],
