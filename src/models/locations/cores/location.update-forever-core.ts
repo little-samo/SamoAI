604233176999a7ceb22c3b6e2b4f32506d0590e8
Lucid <lucid@littlesamo.io>
2025-03-05 19:06:00 +0900

    Entity Updates Changed from Parallel to Sequential

--- src/models/locations/cores/location.update-forever-core.ts
+++ src/models/locations/cores/location.update-forever-core.ts
@@ -6,11 +6,9 @@ export class LocationUpdateForeverCore extends LocationCore {
   public static readonly UPDATE_INTERVAL = 1000;
 
   public async update(): Promise<number> {
-    await Promise.all(
-      Array.from(Object.values(this.location.entities)).map((entity) =>
-        entity.update()
-      )
-    );
+    for (const entity of Object.values(this.location.entities)) {
+      await entity.update();
+    }
     return LocationUpdateForeverCore.UPDATE_INTERVAL;
   }
 }
--- src/models/locations/cores/location.update-once-core.ts
+++ src/models/locations/cores/location.update-once-core.ts
@@ -4,11 +4,9 @@ import { LocationCore } from './location.core';
 @RegisterLocationCore('update_once')
 export class LocationUpdateOnceCore extends LocationCore {
   public async update(): Promise<number> {
-    await Promise.all(
-      Array.from(Object.values(this.location.entities)).map((entity) =>
-        entity.update()
-      )
-    );
+    for (const entity of Object.values(this.location.entities)) {
+      await entity.update();
+    }
     return 0;
   }
 }
