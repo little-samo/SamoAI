dc9d4fb8d9e6b7c2a991dbd47b3ed8cfcece05f0
Lucid <lucid@littlesamo.io>
2025-03-12 11:07:00 +0900

    Clarify GBA movement action description and coordinate constraints

--- src/models/entities/agents/actions/agent.move-gba-action.ts
+++ src/models/entities/agents/actions/agent.move-gba-action.ts
@@ -14,7 +14,7 @@ export class AgentMoveGbaAction extends AgentAction {
     switch (this.version) {
       case 1:
       default:
-        return ' Moves a GBA character to the specified coordinates. The coordinates are given as absolute values, but only those within the visible screen area are valid. When the coordinates are specified, the algorithm will move the character as close as possible to the target position. If no valid path exists on the screen, the character will move to the nearest possible location.';
+        return `Moves a GBA character to the specified coordinates. The coordinates are given as absolute values. When the coordinates are specified, the algorithm will move the character as close as possible to the target position, but pathfinding is limited to the Minimap's range. If no valid path exists on the screen, the character will move to the nearest possible location.`;
     }
   }
 
@@ -25,9 +25,7 @@ export class AgentMoveGbaAction extends AgentAction {
         return z.object({
           destination: z
             .string()
-            .describe(
-              `The absolute coordinates of the destination. Only coordinates within the visible screen area should be specified.`
-            ),
+            .describe(`The absolute coordinates of the destination.`),
         });
     }
   }
