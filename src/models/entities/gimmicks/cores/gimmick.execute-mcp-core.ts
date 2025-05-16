d501437f3437a6b3b0ad83798f70a511bc6b9b3e
Jindo <jindo@littlesamo.io>
2025-05-16 16:33:28 +0900

    fix: remove mcp gimmick core temporary fix

--- src/models/entities/gimmicks/cores/gimmick.execute-mcp-core.ts
+++ src/models/entities/gimmicks/cores/gimmick.execute-mcp-core.ts
@@ -344,11 +344,6 @@ export class GimmickExecuteMcpCore extends GimmickCore {
       return 'Required parameters missing (tool, args)';
     }
 
-    // temporary fix for the issue that the tools are not fetched
-    if (this.tools.length === 0) {
-      await this.fetchAndCacheTools();
-    }
-
     if (!this.tools.includes(tool)) {
       return `Unsupported tool: ${tool}`;
     }
