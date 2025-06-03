06ca212c000775800438d1e83a9f4f24bbff7ecf
Lucid <lucid@littlesamo.io>
2025-06-03 17:54:44 +0900

    Remove unused event listeners and imports

--- src/core/managers/world.manager.ts
+++ src/core/managers/world.manager.ts
@@ -1,10 +1,4 @@
-import {
-  ENV,
-  LlmGenerateResponse,
-  LlmMessage,
-  LlmToolCall,
-  LlmToolsResponse,
-} from '@little-samo/samo-ai/common';
+import { ENV, LlmMessage, LlmToolCall } from '@little-samo/samo-ai/common';
 import { AsyncEventEmitter } from '@little-samo/samo-ai/common';
 import {
   Agent,
@@ -592,34 +586,6 @@ export class WorldManager extends AsyncEventEmitter {
     if (options.preAction) {
       await options.preAction(location);
     }
-
-    location.on(
-      'llmGenerate',
-      async (entity: Entity, response: LlmGenerateResponse) => {
-        await this.emitAsync(
-          'locationLlmGenerate',
-          locationId,
-          entity,
-          response
-        );
-      }
-    );
-
-    location.on(
-      'llmUseTools',
-      async (entity: Entity, response: LlmToolsResponse) => {
-        await this.emitAsync(
-          'locationLlmUseTools',
-          locationId,
-          entity,
-          response
-        );
-      }
-    );
-
-    location.on('agentExecuteNextActions', async (agent: Agent) => {
-      await this.emitAsync('locationAgentExecution', locationId, agent);
-    });
 
     location.on(
       'messageAdded',
