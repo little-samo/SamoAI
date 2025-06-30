591c2405dfeec4d32ae3841a168bb62ba8d4aaa9
Lucid <lucid@littlesamo.io>
2025-06-30 15:12:41 +0900

    Remove PREV_MESSAGE from GimmickParameters type

--- src/models/entities/gimmicks/gimmick.types.ts
+++ src/models/entities/gimmicks/gimmick.types.ts
@@ -2,7 +2,4 @@ import { EntityId } from '../entity.types';
 
 export type GimmickId = EntityId & { __gimmickId: true };
 
-export type GimmickParameters =
-  | 'PREV_MESSAGE'
-  | string
-  | Record<string, unknown>;
+export type GimmickParameters = string | Record<string, unknown>;
