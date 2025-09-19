0601d90b4b5039970ceaa38e9e5839ef078b35dd
Lucid <lucid@littlesamo.io>
2025-09-19 12:03:37 +0900

    Make location state update params required

--- src/core/repositories/location.repository.ts
+++ src/core/repositories/location.repository.ts
@@ -54,12 +54,12 @@ export interface LocationRepository {
   ): Promise<void>;
   updateLocationStateRemainingAgentExecutions(
     locationId: LocationId,
-    value?:
+    value:
       | {
-          remainingAgentExecutions?: number | null;
+          remainingAgentExecutions: number | null;
         }
       | {
-          remainingAgemtExecutionsDelta?: number | null;
+          remainingAgemtExecutionsDelta: number | null;
         }
   ): Promise<void>;
   updateLocationStateCanvas(
